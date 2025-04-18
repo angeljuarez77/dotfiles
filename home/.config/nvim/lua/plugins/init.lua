36c202125d34bd4a79d4c2f33eff360e8fc2202e
Angel Juarez <angeljuarez729@gmail.com>
2025-04-18 01:15:59 -0400

    removed comment

--- home/.config/nvim/lua/plugins/init.lua
+++ home/.config/nvim/lua/plugins/init.lua
@@ -1,7 +1,7 @@
 return {
   {
     "stevearc/conform.nvim",
-    event = 'BufWritePre', -- uncomment for format on save
+    event = "BufWritePre",
     opts = require "configs.conform",
   },
 
@@ -12,8 +12,8 @@ return {
     end,
   },
   {
-  	"nvim-treesitter/nvim-treesitter",
-    opts = require "configs.treesitter"
+    "nvim-treesitter/nvim-treesitter",
+    opts = require "configs.treesitter",
   },
   {
     "unblevable/quick-scope",
